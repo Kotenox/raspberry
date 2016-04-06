5cde6411d37c5abe84a67608d601569e42ffd98f
Kotenox <alex_siyer@hotmail.com>
2016-04-06 00:30:52 -0300

    version 1.1

--- lab1.py
+++ lab1.py
@@ -2,4 +2,6 @@ Python 2.7.9 (default, Mar  8 2015, 00:52:26)
 [GCC 4.9.2] on linux2
 Type "copyright", "credits" or "license()" for more information.
 >>> from SimpleCV import Camera, Display, Image
->>> 
+>>> webcam = Camera()
+>>> foto = webcam.getImage()
+>>> foto.show()
