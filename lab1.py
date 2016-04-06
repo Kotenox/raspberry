e571875c94da57901b0344e881a2abc5ba973dba
Kotenox <alex_siyer@hotmail.com>
2016-04-06 01:33:11 -0300

    V 1.5

--- lab1.py
+++ lab1.py
@@ -2,9 +2,8 @@ Python 2.7.9 (default, Mar  8 2015, 00:52:26)
 [GCC 4.9.2] on linux2
 Type "copyright", "credits" or "license()" for more information.
 >>> from SimpleCV import Camera, Display, Image
->>> time.sleep(10)
->>> webcam = Camera()
->>> time.sleep(10)
->>> foto = webcam.getImage()
->>> time.sleep(10)
+>>> import time
+>>> sleep(10)
+>>> foto = Camera().getImage()
+>>> sleep(10)
 >>> foto.save("myfirst.png")
