3e8d47a85a0e86ca5bdfae133faf1a8daf2ccc32
Kotenox <alex_siyer@hotmail.com>
2016-04-06 12:09:34 -0300

    Version 1.2.3 (Version Final)
    Terminada.
    (En Raspberry) Hacer doble click en el archivo y elegir "Run as module"
    El programa tomara una fotografia con la webcam al ejecutarlo
    
    (En windows) Hacer doble click en el archivo. El programa tomara una
    fotografia  con la webcam al ejecutarlo

--- lab1.py
+++ lab1.py
@@ -1,8 +1,10 @@
 print("Importando")
 from SimpleCV import Camera, Display, Image
+import time
 print("usando Camera()")
 webcam=Camera()
 print("Usando getImage")
+time.sleep(5)
 foto=webcam.getImage()
 print("Salvando Foto")
-foto.save("ohmy.jpg")
+foto.save("Foto.jpg")
