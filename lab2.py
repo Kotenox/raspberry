7a64d75442ff0fe4b2b8b25bdf1dcf83e6b1e42d
Kotenox <alex_siyer@hotmail.com>
2016-04-28 22:54:25 -0300

    V 3

--- lab2.py
+++ lab2.py
@@ -1,5 +1,7 @@
 print("Importando")
+import matplotlib.pyplot as plt
 from SimpleCV import Camera, Image, time
+
 print("Usando Camera")
 webcam=Camera()
 print("Usando getImage")
@@ -7,6 +9,7 @@ time.sleep(5)
 foto=webcam.getImage()
 print("Salvando Foto Original")
 foto.save("Foto.jpg")
+
 print("Salvando Foto en escala de Grises")
 fotogris=foto.grayscale()
 fotogris.save("Foto_Gris.jpg")
@@ -18,5 +21,16 @@ print("Salvando Foto dividida en canales")
 rojo.save("Foto_rojo.jpg")
 verde.save("Foto_verde.jpg")
 azul.save("Foto_azul.jpg")
+
+print("Generando Histogramas")
+histgris=fotogris.histogram(255)
+histrojo=rojo.histogram(255)
+histverde=verde.histogram(255)
+histazul=azul.histogram(255)
+
+plt.plot(histgris)
+plt.plot(histrojo)
+plt.plot(histverde)
+plt.plot(histazul)
 
 print("Listo!")
