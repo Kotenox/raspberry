857e5a0d5d897c02895a83e9368f43937699fc5d
Kotenox <alex_siyer@hotmail.com>
2016-04-29 00:35:18 -0300

    laboratorio 2
    Abrir con IDLE
    luego, Run as Module

--- lab2.py
+++ lab2.py
@@ -1,4 +1,4 @@
-print("Importando")
+print("Importando, espere...")
 import matplotlib.pyplot as plt
 from SimpleCV import Camera, Image, time
 
@@ -8,19 +8,21 @@ print("Usando getImage")
 time.sleep(5)
 foto=webcam.getImage()
 print("Salvando Foto Original")
-foto.save("Foto.jpg")
+foto.save("Original.jpg")
 
 print("Salvando Foto en escala de Grises")
 fotogris=foto.grayscale()
-fotogris.save("Foto_Gris.jpg")
-print("Salvando Foto Binaria")
-fotobinaria=foto.binarize()
-fotobinaria.save("Foto_Binaria.jpg")
+fotogris.save("Grises.jpg")
+
 print("Salvando Foto dividida en canales")
 (rojo,verde,azul)=foto.splitChannels()
-rojo.save("Foto_rojo.jpg")
-verde.save("Foto_verde.jpg")
-azul.save("Foto_azul.jpg")
+rojo.save("Rojo.jpg")
+verde.save("Verde.jpg")
+azul.save("Azul.jpg")
+
+print("Salvando Foto Binaria")
+fotobinaria=foto.binarize()
+fotobinaria.save("Binaria.jpg")
 
 print("Generando Histogramas")
 histgris=fotogris.histogram(255)
@@ -28,9 +30,29 @@ histrojo=rojo.histogram(255)
 histverde=verde.histogram(255)
 histazul=azul.histogram(255)
 
+print("Salvando Histogramas")
+fig=plt.figure()
 plt.plot(histgris)
+plt.title("Gris")
+plt.show()
+fig.savefig("Hist_Gris.png")
+
+fig=plt.figure()
 plt.plot(histrojo)
+plt.title("Rojo")
+plt.show()
+fig.savefig("Hist_Rojo.png")
+
+fig=plt.figure()
 plt.plot(histverde)
+plt.title("Verde")
+plt.show()
+fig.savefig("Hist_Verde.png")
+
+fig=plt.figure()
 plt.plot(histazul)
+plt.title("Azul")
+plt.show()
+fig.savefig("Hist_Azul.png")
 
 print("Listo!")
